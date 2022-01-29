a75ee8d70457c3197e91a85e4ff8c75b5d7f9638
Serhii Tsymbalenko <s.tsymbalenko@gmail.com>
2022-01-29 16:12:19 +0200

    add day8

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,57 @@
+package day8;
+
+public class Airplane {
+    String manufacturer; //(изготовитель)
+    int year; //(год выпуска)
+    int length; //(длина)
+    int weight; //(вес)
+    int fuel = 0; //(количество топлива в баке)
+
+    public Airplane(String manufacturer, int year, int length, int weight) {
+        this.manufacturer = manufacturer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public void setFuel(int fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void info(){
+        System.out.println("Изготовитель: " + manufacturer + ", год выпуска: " + year + ", длина: " + length + ", вес: " + weight + ", количество топлива в баке: " + fuel);
+    }
+
+    public int fillUp(int n){
+        return fuel = fuel+n;
+    }
+
+    @Override
+    public String toString() {
+        return "Изготовитель: " + manufacturer +
+                ", год выпуска: " + year +
+                ", длина: " + length +
+                ", вес: " + weight +
+                ", количество топлива в баке: " + fuel;
+    }
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+        String str = "0";
+        for( int i = 1; i <=2000; i++){
+            str = str + " " + i;
+        }
+        System.out.println(str);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime - startTime));
 
+        long startTime2 = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i = 0; i <=2000; i++){
+            stringBuilder.append(i + " ");
+        }
+        System.out.println(stringBuilder);
+        long stopTime2 = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.: " + (stopTime2 - startTime2));
     }
 }
