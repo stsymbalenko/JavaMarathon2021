9224f34eae0f9de8021ae79782f8dcfb3d4a4400
Serhii Tsymbalenko <s.tsymbalenko@gmail.com>
2022-02-14 23:33:03 +0200

    add day16

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,8 +1,34 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("file.txt");
+        printResult(file);
+    }
 
+    public static void printResult(File file){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line  = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        int counter = 0;
+        int sum = 0;
+
+        for(String number:numbersString){
+            sum = sum + Integer.parseInt(number);
+            counter++;
+        }
+
+        System.out.print((double) sum/counter + " --> ");
+        System.out.printf("%.3f", (double) sum/counter);
     }
 }
 
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,80 @@
 package day16;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file1 = new File("file1.txt");
+        File file2 = new File("file2.txt");
 
+        PrintWriter printWriter1 = null;
+        try {
+            printWriter1 = new PrintWriter(file1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        int ch = 0;
+        for (int i = 0; i < 1000; i++) {
+            ch= (int)(Math.random() * 100);
+            printWriter1.print(ch);
+            printWriter1.print(" ");
+        }
+        printWriter1.close();
+
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String line = scanner.nextLine();
+            String[] numbersString = line.split(" ");
+            int counter = 0;
+            double sum = 0;
+
+        PrintWriter printWriter2 = null;
+        try {
+            printWriter2 = new PrintWriter(file2);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+            for (String number : numbersString) {
+                if (counter < 20) {
+                    sum = sum + Double.parseDouble(number);
+                    counter++;
+                } else {
+                    printWriter2.print(sum / 20);
+                    printWriter2.print(" ");
+                    counter = 0;
+                    sum = 0;
+                    sum = sum + Double.parseDouble(number);
+                    counter++;
+                }
+            }
+            printWriter2.close();
+
+            printResult(file2);
+        }
+
+
+    public static void printResult(File file){
+        double sum = 0;
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String line = scanner.nextLine();
+        String[] numbersString = line.split(" ");
+        for (String number : numbersString) {
+                sum = sum + Double.parseDouble(number);
+        }
+        System.out.println((int)sum);
     }
 }
